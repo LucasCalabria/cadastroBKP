bcd1e9584ddf4370e22e93862b78f70b2788c081
Lucas Calabria <lucascalabria@ymail.com>
2021-12-14 21:34:30 -0300

    first commit Cadastro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//import { Container, Typography } from "@material-ui/core"
+import AddPage from "./pages/addPage";
+import DashPage from "./pages/dashPage";
+import {Route, Link, Routes} from 'react-router-dom';
+
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path = "/" element={<DashPage />} />
+        <Route exact path = "/add" element={<AddPage />} />
+      </Routes>
     </div>
   );
 }
